9c0b52f1f75f75df40f83a908c578bda19663b28
Sohum Chatterjee <sohumc@microsoft.com>
2016-04-25 22:58:55 +0000

    PR 30840: Added Text Shimmer Sample
    Added Text Shimmer Sample

--- SampleGallery/Shared/SampleDefinition.cs
+++ SampleGallery/Shared/SampleDefinition.cs
@@ -83,6 +83,7 @@ namespace CompositionSampleGallery
             new SampleDefinition(PhotoViewer.StaticSampleName,                  typeof(PhotoViewer),                    SampleType.EndToEnd,  SampleCategory.Effects),
             new SampleDefinition(PullToAnimate.StaticSampleName,                typeof(PullToAnimate),                  SampleType.EndToEnd,  SampleCategory.ExpressionAnimations),
             new SampleDefinition(ShadowPlayground.StaticSampleName,             typeof(ShadowPlayground),               SampleType.Reference, SampleCategory.Visuals),
+            new SampleDefinition(TextShimmer.StaticSampleName,                  typeof(TextShimmer),                    SampleType.EndToEnd,  SampleCategory.Effects),
             new SampleDefinition(ThumbnailLighting.StaticSampleName,            typeof(ThumbnailLighting),              SampleType.EndToEnd,  SampleCategory.Effects),
 #endif
 
