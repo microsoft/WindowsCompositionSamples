106e4b03ca58624956e92091f784f9a9a9ca401c
Jacob Stern <jaster@microsoft.com>
2017-02-06 11:58:16 -0800

    Remove unused imports

--- SampleGallery/Samples/SDK Insider/NavigationFlow/NavigationFlowSourcePage.xaml.cs
+++ SampleGallery/Samples/SDK Insider/NavigationFlow/NavigationFlowSourcePage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Composition;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Hosting;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
