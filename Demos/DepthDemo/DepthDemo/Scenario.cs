5b7e9a1b3d0c45273ca4a0c4cf9b596f3579abf3
Danielle Neuberger <daneuber@microsoft.com>
2017-06-08 19:49:12 +0000

    Merged PR 507209: Merge daneuber/DepthPrototypeMerge to future

---
+++ Demos/DepthDemo/DepthDemo/Scenario.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace DepthDemo
+{
+    public abstract class Scenario
+    {
+        public Scenario(int identifier)
+        {
+            Identifier = identifier;
+            IsActive = false;
+        }
+
+        public struct LayerConfig
+        {
+            public int ID;
+            public Color Color;
+            public Vector3 Scale;
+            public int ShadowBlurRadius;
+            public Vector3 ShadowOffset;
+
+            public LayerConfig(int id, Color color, Vector3 scale, int shadowBlurRadius, Vector3 shadowOffset)
+            {
+                ID = id;
+                Color = color;
+                Scale = scale;
+                ShadowBlurRadius = shadowBlurRadius;
+                ShadowOffset = shadowOffset;
+            }
+        }
+
+        /// <summary>
+        /// String describing how to interact with the scenario
+        /// </summary>
+        public string HelpTextInstructions { get; set; }
+
+        /// <summary>
+        /// Boolean indicating whether the scenario is active or not. 'Active' indicates
+        /// the end user can see the scenario in the app.
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Identifier for the scenario
+        /// </summary>
+        public int Identifier { get; set; }
+
+        /// <summary>
+        /// Used to define new behavior when animating a visual from one conceptual layer to another,
+        /// on a per scenario basis.
+        /// </summary>
+        public virtual void AnimateVisualToLayer(Layer oldLayer, Layer newLayer, SpriteVisual targetVisual, bool overrideProjectedShadows = false)
+        {
+            if (oldLayer.LayerBackingVisual.Children.Contains(targetVisual))
+            {
+                // Remove visual from current layer
+                oldLayer.LayerBackingVisual.Children.Remove(targetVisual);
+
+                // Add to new layer
+                newLayer.LayerBackingVisual.Children.InsertAtTop(targetVisual);
+
+                // Trigger animation to new values
+                newLayer.AnimateAddedVisual(targetVisual);
+            }
+        }
+
+        public Visual GetHittestVisual(Point pointerPosition, List<Visual> visuals, List<Layer> layers, List<SpriteVisual> excludedVisuals)
+        {
+            return GetHittestVisualHelper(pointerPosition, visuals, layers, excludedVisuals);
+        }
+
+        public Visual GetHittestVisual(Point pointerPosition, List<SpriteVisual> visuals, List<Layer> layers, List<SpriteVisual> excludedVisuals)
+        {
+            List<Visual> visualsList = new List<Visual>();
+            foreach (Visual visual in visuals)
+            {
+                visualsList.Add((Visual)visual);
+            }
+
+            return GetHittestVisualHelper(pointerPosition, visualsList, layers, excludedVisuals);
+        }
+
+        public Visual GetHittestVisual(Point pointerPosition, VisualCollection visuals, List<Layer> layers, List<SpriteVisual> excludedVisuals)
+        {
+            List<Visual> visualsList = new List<Visual>();
+            foreach (Visual visual in visuals)
+            {
+                visualsList.Add(visual);
+            }
+
+            return GetHittestVisualHelper(pointerPosition, visualsList, layers, excludedVisuals);
+        }
+
+        private Visual GetHittestVisualHelper(Point pointerPosition, List<Visual> visuals, List<Layer> layers, List<SpriteVisual> excludedVisuals)
+        {
+            if (excludedVisuals == null) { excludedVisuals = new List<SpriteVisual>(); }
+            List<Visual> hitVisuals = new List<Visual>();
+            foreach (Visual visual in visuals)
+            {
+                if (!excludedVisuals.Contains(visual))
+                {
+                    var visualXLowerBound = visual.Offset.X;
+                    var visualXUpperBound = visual.Offset.X + visual.Size.X;
+                    var visualYLowerBound = visual.Offset.Y;
+                    var visualYUpperBound = visual.Offset.Y + visual.Size.Y;
+
+                    // Check if clicked
+                    if (pointerPosition.X >= visualXLowerBound && pointerPosition.X <= visualXUpperBound &&
+                    pointerPosition.Y >= visualYLowerBound && pointerPosition.Y <= visualYUpperBound)
+                    {
+                        hitVisuals.Add(visual);
+                    }
+                }
+            }
+
+            if (hitVisuals.Count >= 1)
+            {
+                if (hitVisuals.Count == 1)
+                {
+                    return hitVisuals.First();
+                }
+                else
+                {
+                    // Go through each layer starting at the end (highest) and get the topmost hit visual
+                    for (int i = layers.Count - 1; i >= 0; i--)
+                    {
+                        foreach (Visual v in hitVisuals)
+                        {
+                            if (layers[i].LayerBackingVisual.Children.Contains(v))
+                            {
+                                // Convert back to SV or CV
+                                if (v.GetType().Name.Equals("SpriteVisual"))
+                                {
+                                    return (SpriteVisual)v;
+                                }
+                                else if (v.GetType().Name.Equals("ContainerVisual"))
+                                {
+                                    return (ContainerVisual)v;
+                                }
+                                return v;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Given a visual, get it's parent Layer object
+        /// </summary>
+        public Layer GetParentLayer(List<Layer> layers, Visual visual)
+        {
+            foreach (Layer layer in layers)
+            {
+                if (layer.LayerBackingVisual.Children.Contains(visual))
+                {
+                    return layer;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Create conceptual layers for the scenario, in order to define layer-specific behavior
+        /// </summary>
+        abstract public void CreateLayers();
+
+        /// <summary>
+        /// Size changed listener helper method
+        /// </summary>
+        abstract public void SizeChanged();
+
+        /// <summary>
+        /// Used to actually create the components of the scenario. Separated from the constructor
+        /// in order to provide greater control over scenarios.
+        /// </summary>
+        abstract public void ImplementScenario(Compositor compositor, SpriteVisual scenarioContainer);
+
+        /// <summary>
+        /// Scenario-specific behavior for canvas pointer moved input
+        /// </summary>
+        virtual public void CanvasPointerMoved(object sender, PointerRoutedEventArgs e, Canvas canvasReference) { }
+
+        /// <summary>
+        /// Scenario-specific behavior for canvas pointer pressed input
+        /// </summary>
+        virtual public void CanvasPointerPressed(object sender, PointerRoutedEventArgs e, Canvas canvasReference) { }
+
+        /// <summary>
+        /// Scenario-specific behavior for canvas double tapped input
+        /// </summary>
+        virtual public void CanvasDoubleTapped(object sender, DoubleTappedRoutedEventArgs e, Canvas canvasReference) { }
+
+    }
+}
---
+++ Demos/DepthDemo/DepthDemo/Scenarios/NestedScenario.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace DepthDemo.Scenarios
+{
+    class NestedScenario : Scenario
+    {
+        private Compositor _compositor;
+        private List<SpriteVisual> _visuals;
+        private List<Layer> _layers;
+        private SpriteVisual _scenarioContainer;
+        private Canvas _canvasReference;
+
+        // Focus treatment increase for top layer objects
+        public static float _focusScaleIncreaseFactor = 1.2f;
+        // Shadow specific
+        public static int _focusShadowBlurRadiusIncreaseAmount = 5;
+        public static int _focusShadowOffsetIncreaseAmount = 10;
+
+        private static List<LayerConfig> s_layers = new List<LayerConfig>
+        {
+            new LayerConfig(-2, Color.FromArgb(255,0,153,153), new Vector3(1, 1, 1), 0, new Vector3(0, 0, 0)),
+            new LayerConfig(-1, Color.FromArgb(255,0,204,204), new Vector3(1, 1, 1), 5, new Vector3(5,5,-5)),
+            new LayerConfig(0, Color.FromArgb(255,0,255,255), new Vector3(1, 1, 1), 9, new Vector3(10,10,-10)),
+            new LayerConfig(1, Color.FromArgb(255,102,255,255), new Vector3(1, 1, 1), 15, new Vector3(15,15,-15)),
+            new LayerConfig(2, Color.FromArgb(255,204,255,255), new Vector3(1, 1, 1), 30, new Vector3(20,20,-20))
+        };
+
+        public NestedScenario(int identifier, Compositor compositor, Canvas canvasReference) : base(identifier)
+        {
+            _visuals = new List<SpriteVisual>();
+            _layers = new List<Layer>();
+
+            _compositor = compositor;
+            _canvasReference = canvasReference;
+
+            HelpTextInstructions = "1. Move your finger L/R across the screen as if panning, to trigger the parallax behavior in the nested visuals.";
+        }
+
+        public override void CreateLayers()
+        {
+            // Create layers
+            for (int i = 0; i < s_layers.Count; i++)
+            {
+                var offset = new Vector3(0, 0, (ConfigurationConstants.ZOffsetSpacingIncrement * s_layers[i].ID));
+                var size = new Vector2((float)_canvasReference.ActualWidth, (float)_canvasReference.ActualHeight);
+
+                Layer l = new Layer(_compositor, s_layers[i].ID, offset, size, _scenarioContainer, _compositor.CreateColorBrush(s_layers[i].Color));
+                _layers.Add(l);
+
+                // Set depth treatment
+                ShadowTreatment shadowTreatment = new ShadowTreatment(s_layers[i].ShadowBlurRadius, s_layers[i].ShadowOffset, _focusShadowBlurRadiusIncreaseAmount, _focusShadowOffsetIncreaseAmount);
+                DepthTreatmentConfigurations depthTreatment = new DepthTreatmentConfigurations(_layers[i], s_layers[i].Scale, _focusScaleIncreaseFactor, shadowTreatment);
+                _layers[i].SetDepthTreatments(depthTreatment);
+            }
+        }
+
+        public override void ImplementScenario(Compositor compositor, SpriteVisual scenarioContainer)
+        {
+            _scenarioContainer = scenarioContainer;
+
+            CreateLayers();
+
+            // Create basic sprite visual to add to each layer, with random colors
+            for (int i = 0; i < _layers.Count; i++)
+            {
+                SpriteVisual v = compositor.CreateSpriteVisual();
+                v.Brush = compositor.CreateColorBrush(s_layers[i].Color);
+                v.Opacity = 1.0f;
+
+                _visuals.Add(v);
+                _layers[i].LayerBackingVisual.Children.InsertAtTop(v);
+            }
+
+            UpdateSizeAndLayout();
+
+            foreach (Layer l in _layers)
+            {
+                l.RefreshApplyDepthTreatments();
+            }
+        }
+
+        public List<SpriteVisual> GetVisuals()
+        {
+            return _visuals;
+        }
+
+        public override void SizeChanged()
+        {
+            UpdateSizeAndLayout();
+        }
+
+        private void UpdateSizeAndLayout()
+        {
+            // Incremental offset and size starting values
+            var previousOffset = new Vector3(10, 10, 0);
+            var previousSize = new Vector2((float)_scenarioContainer.Size.X - 200, (float)_scenarioContainer.Size.Y - 200);
+
+            for (int i = 0; i < _layers.Count; i++)
+            {
+                foreach (SpriteVisual child in _layers[i].LayerBackingVisual.Children)
+                {
+                    child.Size = previousSize;
+                    child.Offset = previousOffset;
+                    child.CenterPoint = new Vector3(previousSize.X / 2, previousSize.Y / 2, 0);   // to center scaling
+
+                    previousOffset = new Vector3(previousOffset.X + 50, previousOffset.Y + 50, 0);
+                    previousSize = new Vector2(previousSize.X - 100, previousSize.Y - 100);
+                }
+            }
+        }
+    }
+}
