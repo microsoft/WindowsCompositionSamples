bd69764d79d80bbd7a9317c28291db5c3ad217a6
Danielle Neuberger <daneuber@microsoft.com>
2017-09-13 22:20:01 +0000

    Merged PR 769745: Add copyright blob where missing
    Add copyright blob where missing
    
    Does not include SDKInsider samples folder

--- SampleGallery/Samples/SDK 14393/NineGridResizing/NineGridScenarios/SurfaceNineGridScenario.cs
+++ SampleGallery/Samples/SDK 14393/NineGridResizing/NineGridScenarios/SurfaceNineGridScenario.cs
@@ -1,4 +1,18 @@
-﻿using Windows.UI.Composition;
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//*********************************************************
+
+using Windows.UI.Composition;
 
 namespace CompositionSampleGallery.Samples.SDK_14393.NineGridResizing.NineGridScenarios
 {
